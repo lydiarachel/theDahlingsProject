b21f733db9905556d2c2c6721b825728ffbe39d7
Nataliia Frank <nataliiafrank@gmail.com>
2018-08-10 13:11:04 -0400

    Added cards to homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,28 @@
 import React from "react";
 import SearchBar from "../../components/SearchBar";
+import Heading from "../../components/Heading";
+import ViewCard from "../../components/ViewCard";
+import "./Home.css";
 
 const Home = () => (
-    <div>
-      <h2>Home</h2>
+  <div>
+    <div className="search-box">
       <SearchBar />
     </div>
+    
+    <div className="title">
+      <Heading>Top Rated Gists</Heading>
+    </div>
+    
+    <div className="row">
+      <ViewCard />
+      <ViewCard />
+      <ViewCard />
+      <ViewCard />
+      <ViewCard />
+      <ViewCard />
+    </div>
+  </div>
 );
 
 export default Home;
