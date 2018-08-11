1fdd0cc05d94d364c71ec0b37d1f4ccc806625d2
Nataliia Frank <nataliiafrank@gmail.com>
2018-08-10 22:52:25 -0400

    Fixed for attr in searchbar js and added meadiea queries

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Heading from './components/Heading'
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import About from "./pages/About";
