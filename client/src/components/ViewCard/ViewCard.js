b952bd172d760f5f34cf65bd3e6ff719c778bde0
Nataliia Frank <nataliiafrank@gmail.com>
2018-08-15 19:55:50 -0400

    Wrote function for populating homepage with gists

--- client/src/components/ViewCard/ViewCard.js
+++ client/src/components/ViewCard/ViewCard.js
@@ -5,14 +5,17 @@ import { Link } from "react-router-dom";
 const ViewCard = props => (
         <div className="col s12 m6 l4">
           <div className="card medium">
-            <div className="card-content white-text grey lighten-1">
-              <span className="card-title">
-                <Link to="/gist-page">
-                  Gist Tile
-                </Link>
-              </span>
-            </div>
             <div className="card-content white">
+              <h5><Link to="/gist-page">
+                  {props.title}
+                </Link>
+              </h5>
+              
+              <p>Author: {props.author}</p>
+              <p>Category: {props.category.charAt(0).toUpperCase()+ props.category.slice(1)}</p>
+              <p><i className="tiny material-icons search-icon">thumb_up</i> {props.likes}</p>
+              <p>Created at: {props.date}</p>
+            
             </div>
           </div>
         </div>
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,31 +3,59 @@ import SearchBar from "../../components/SearchBar";
 import Heading from "../../components/Heading";
 import ViewCard from "../../components/ViewCard";
 import ActionButtons from "../../components/ActionButtons";
+import API from "../../utils/API";
 import "./Home.css";
 
-const Home = () => (
-  <div>
-    <div className="search-box">
-      <SearchBar />
-    </div>
-    
-    <div className="action-buttons">
-      <ActionButtons />
-    </div>
+class Home extends React.Component {
+  state = {
+    results: []
+  }
+  componentDidMount() {
+    API.findGists()
+      .then(result => {
+        console.log(result.data);
+        this.setState({
+          results: result.data
+        })
+      })
+      .catch(err => console.log(err));
+  }
 
-    <div className="title">
-      <Heading>Top Rated Gists</Heading>
-    </div>
-    
-    <div className="row">
-      <ViewCard />
-      <ViewCard />
-      <ViewCard />
-      <ViewCard />
-      <ViewCard />
-      <ViewCard />
-    </div>
-  </div>
-);
+  render() {
+    if (this.state.results.length === 0) {
+      return null;
+    }
+
+    return (
+        <div>
+        <div className="search-box">
+          <SearchBar />
+        </div>
+        
+        <div className="action-buttons">
+          <ActionButtons />
+        </div>
+
+        <div className="title">
+          <Heading>Top Rated Gists</Heading>
+        </div>
+        
+        <div className="row">
+          {
+            this.state.results.map(result => (
+              <ViewCard title={result.title}
+              author={result.author}
+              category={result.category}
+              likes={result.liked}
+              date={result.date}
+              key={result._id}
+              />
+            ))
+          }
+        </div>
+      </div>
+    )
+  } 
+}
 
 export default Home;
