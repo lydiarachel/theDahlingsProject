df1a0e077fcbe1f1cd6ea1ac2c3e1925208b2b04
Nataliia Frank <nataliiafrank@gmail.com>
2018-08-17 10:15:51 -0400

    Added Create-profile page

---
+++ client/src/pages/CreateProfile/CreateProfile.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import SearchBar from "../../components/SearchBar";
+import Heading from "../../components/Heading";
+import UserForm from "../../components/UserForm";
+class CreateProfile extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <div className="title">
+          <Heading>Create User Profile</Heading>
+        </div>
+        <div className="row">
+          <UserForm />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CreateProfile;
