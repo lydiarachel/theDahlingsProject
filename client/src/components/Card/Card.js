9cfda5eb942b065b254ac4fb93e82f5a4ac31be4
lydiarachel <lydiarachel@gmail.com>
2018-08-18 10:11:10 -0400

    hard coded author id, and added some styling for gist page, but nnot much

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,14 +1,16 @@
 import React from "react";
+import './Card.css'
 
 const Card = props =>(
  
       <div className="row">
         <div className="col s11 m11">
           <div className="card small">
-           <div className="card-content white-text grey lighten-1">
-              <span className="card-title">{props.title}</span>
-           </div>
           <div className="card-content white">
+          <span className="card-title">{props.title}</span>
+          {/*create a div that will have two p tags with className that are styled. This will allow me to place date and by 
+          information on the card. need to write a props.date.  
+          for body, need to have a div with p tag and className that is styled for the body of the*/}
           {props.body}
           {props.liked}
           </div>
